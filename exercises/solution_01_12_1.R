c974f128c6cd955627512be0bff37e2386fddd4f
Julia Silge <julia.silge@gmail.com>
2020-05-04 10:33:44 -0600

    Use collect_predictions()

--- exercises/solution_01_12_1.R
+++ exercises/solution_01_12_1.R
@@ -5,10 +5,10 @@ lm_res <- readRDS("data/c1_lm_res.rds")
 rf_res <- readRDS("data/c1_rf_res.rds")
 
 results <-  bind_rows(lm_res %>%
-                          unnest(.predictions) %>%
+                          collect_predictions() %>%
                           mutate(model = "lm"),
                       rf_res %>%
-                          unnest(.predictions) %>%
+                          collect_predictions() %>%
                           mutate(model = "rf"))
 
 results
