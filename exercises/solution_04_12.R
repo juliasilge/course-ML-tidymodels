4f6543a7fdd5e49a38c1d7aa0042a49da92f48e8
Julia Silge <julia.silge@gmail.com>
2020-05-12 17:23:12 -0600

    Use a decision tree, not kknn. Addresses #4.

--- exercises/solution_04_12.R
+++ exercises/solution_04_12.R
@@ -1,4 +1,3 @@
-library(tidyverse)
 library(tidymodels)
 
 sisters_other <- readRDS("data/c4_other.rds")
@@ -8,16 +7,16 @@ sisters_recipe <- recipe(age ~ ., data = sisters_other) %>%
     step_normalize(all_predictors()) %>%
     step_pca(all_predictors(), num_comp = tune())
 
-knn_spec <- nearest_neighbor(
-    neighbors = tune(),
-    dist_power = tune()
-) %>%
-    set_engine("kknn") %>%
+tree_spec <- decision_tree(
+    cost_complexity = tune(),
+    tree_depth = tune()
+) %>% 
+    set_engine("rpart") %>% 
     set_mode("regression")
 
-knn_wf <- workflow() %>%
+tree_wf <- workflow() %>%
     add_recipe(sisters_recipe) %>%
-    add_model(knn_spec)
+    add_model(tree_spec)
 
 tree_grid <- grid_regular(num_comp(c(3, 12)),
                           cost_complexity(),
